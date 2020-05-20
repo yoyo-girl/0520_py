2f67f3b42a0cda6960ae7137e30d4d0acce0705d
股市小子 <64961116+yoyo-girl@users.noreply.github.com>
2020-05-20 20:17:43 +0800

    Update test_0519.py

--- test_0519.py
+++ test_0519.py
@@ -1,13 +1,14 @@
-import pymysql, requests,time, aaa, json
+import pymysql, requests,time, random, aaa, json
 #import stockid
 from bs4 import BeautifulSoup
 from datetime import datetime
-#建立MySQL的連線
-# db = pymysql.connect(host='localhost', user='dbuser', passwd='aabb1234', db='Project_test', port=3306, charset='utf8')
-# cursor = db.cursor() #建立游標
-# db.autocommit(True)
 
-T=(aaa.create_assist_date("20200518"))
+#建立MySQL的連線
+db = pymysql.connect(host='localhost', user='dbuser', passwd='aabb1234', db='Project_test', port=3306, charset='utf8')
+cursor = db.cursor() #建立游標
+db.autocommit(True)
+
+T=(aaa.create_assist_date())
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36'}
 
 for t in T:
@@ -20,26 +21,28 @@ for t in T:
     date = jdata['date']
 
     for j in range(0,len(jdata['data'])):
-        for i in (0,4,10,11):
-            stockiid = jdata['data'][j][0]
-            foreign_investment = jdata['data'][j][4]
-            investment_bank = jdata['data'][j][10]
-            local_company = jdata['data'][j][11]
-            ddd = datetime.strptime(str(date), '%Y%m%d').strftime('%Y-%m-%d')
-
+        stockiid = jdata['data'][j][0]
+        foreign_investment = jdata['data'][j][4]
+        investment_bank = jdata['data'][j][10]
+        local_company = jdata['data'][j][11]
+        ddd = datetime.strptime(str(date), '%Y%m%d').strftime('%Y-%m-%d')
+        if len(stockiid) == 4:
             abc = [stockiid, ddd, foreign_investment, investment_bank, local_company]
             abc[2] = (abc[2].replace(',', ''))
             abc[3] = (abc[3].replace(',', ''))
             abc[4] = (abc[4].replace(',', ''))
+            print(abc)
 
-        print(abc)
+            try:
+                cursor.execute('INSERT INTO margin_trading_short_selling(stockiid, date, foreign_investment, investment_bank, local_company)' \
+                                       '' 'VALUES(%s, %s, %s, %s, %s)', abc)
 
-        
-# #     try:
-# #            cursor.execute('INSERT INTO margin_trading_short_selling(stockiid, date, foreign_investment, investment_bank, local_company)' \
-# #                            '' 'VALUES(%s, %s, %s, %s, %s)', abc)
-# #     except Exception as err:
-# #         print(err.args)
+            except Exception as err:
+                print(err.args)
+
+        # sleep_time = random.randint(2, 5) + random.random()
+        # time.sleep(sleep_time)
+
 # # db.commit()
 # # cursor.close()
-# # print('Done')
+print('Done')
